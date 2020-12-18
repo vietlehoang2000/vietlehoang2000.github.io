e565e0e4f0cdcc14776106cbe1f81c7567d44ad7
Vivian <vietlehoang2000@gmail.com>
2020-12-19 00:02:03 +0700

    chiu

---
+++ Web_nang_cao/BTB6/minimoClone/minimo.js
@@ -0,0 +1,24 @@
+let x = 0;
+
+function openNav() {
+    var mediaQuery = window.matchMedia("(max-width: 450px)");
+    if (mediaQuery.matches && x % 2 == 0) {
+      document.getElementsByClassName("headerRightBlock")[0].style.display =
+        "none";
+    } else
+      document.getElementsByClassName("headerRightBlock")[0].style.display =
+        "block";
+    x++;
+}
+
+
+function checkScreen() {
+    var mediaQuery = window.matchMedia("(min-width: 450px)");
+    if (mediaQuery.matches) {
+        document.getElementsByClassName("headerRightBlock")[0].style.display =
+          "block";
+    }
+}
+
+
+document.body.onresize = function () { checkScreen() };
