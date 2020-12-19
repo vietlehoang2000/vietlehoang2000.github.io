6312314b777c9efe28ba113912942065d4703227
Vivian <vietlehoang2000@gmail.com>
2020-12-19 18:11:34 +0700

    change

--- Web_nang_cao/BTB6/minimoClone/minimo.js
+++ Web_nang_cao/BTB6/minimoClone/minimo.js
@@ -1,7 +1,7 @@
 let x = 0;
 
 function openNav() {
-    var mediaQuery = window.matchMedia("(max-width: 450px)");
+    var mediaQuery = window.matchMedia("(max-width: 510px)");
     if (mediaQuery.matches && x % 2 == 0) {
       document.getElementsByClassName("headerRightBlock")[0].style.display =
         "none";
@@ -13,7 +13,7 @@ function openNav() {
 
 
 function checkScreen() {
-    var mediaQuery = window.matchMedia("(min-width: 450px)");
+    var mediaQuery = window.matchMedia("(min-width: 510px)");
     if (mediaQuery.matches) {
         document.getElementsByClassName("headerRightBlock")[0].style.display =
           "block";
