af06b6d68af76c017c2199b479099b50fcaa55c9
Vivian <vietlehoang2000@gmail.com>
2020-12-31 11:33:15 +0700

    change

--- Web_nang_cao/BTB7/zouscript.js
+++ Web_nang_cao/BTB7/zouscript.js
@@ -4,5 +4,36 @@ $(document).ready(function () {
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 1,
+      arrows: true,
+      centerMode: true,
+      centerPadding: "0",
+      prevArrow: ".prevArrow",
+      nextArrow: ".nextArrow",
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: "40px",
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: "40px",
+            slidesToShow: 1,
+          },
+        },
+      ],
     });
 });
+
+// $(function () {
+//   $(".multiple-items").slick({
+//     prevA
+//   })
+// })
