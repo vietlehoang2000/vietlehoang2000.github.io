e4f5fa0aaee70231777b1966a4f9c455130cbb74
Vivian <vietlehoang2000@gmail.com>
2021-03-03 21:21:48 +0700

    c

--- cocacola/js/cocaType.js
+++ cocacola/js/cocaType.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
  $(".multiple-items").slick({
-   dots: true,
+   dots: false,
    infinite: true,
    speed: 300,
    slidesToShow: 3,
    slidesToScroll: 1,
    arrows: true,
+   prevArrow: ".prevArrow",
+   nextArrow: ".nextArrow",
+   centerMode: true,
+   centerPadding: "0px",
    responsive: [
      {
        breakpoint: 1024,
@@ -13,14 +17,13 @@ $(document).ready(function () {
          slidesToShow: 3,
          slidesToScroll: 3,
          infinite: true,
-         dots: true,
        },
      },
      {
-       breakpoint: 600,
+       breakpoint: 1000,
        settings: {
          slidesToShow: 2,
-         slidesToScroll: 2,
+         slidesToScroll: 1,
        },
      },
      {
@@ -30,9 +33,59 @@ $(document).ready(function () {
          slidesToScroll: 1,
        },
      },
-     // You can unslick at a given breakpoint now by adding:
-     // settings: "unslick"
-     // instead of a settings object
    ],
  });
 });
+
+
+function open_nutri() {
+  document.getElementById("test").style.color = "red";
+  document.getElementsByClassName("thirdBanner-des-right-title")[0].style.visibility = "visible";
+  document.getElementsByClassName(
+    "thirdBanner-des-right-title"
+  )[1].style.visibility = "hidden";
+}
+
+function open_ingre(){document.getElementsByClassName(
+  "thirdBanner-des-right-title"
+)[0].style.visibility = "hidden";
+document.getElementsByClassName(
+  "thirdBanner-des-right-title"
+)[1].style.visibility = "visible";
+}
+
+var i =0;
+
+function change_bg_next() {
+  if(i==3)
+    i = 0;
+  else
+  i++;
+  document.getElementsByClassName("main--container-thirdBanner")[i].style.display = "unset";
+  for (a = 0; a <= 3; a++)
+  {
+    if (a != i)
+    {
+      document.getElementsByClassName("main--container-thirdBanner")[a].style.display="none";
+      }
+    }
+}
+
+function change_bg_prev() {
+  if (i == 0)
+    i = 3;
+  else
+    i--;
+  
+  document.getElementsByClassName("main--container-thirdBanner")[
+    i
+  ].style.display = "unset";
+
+   for (a = 3; a >= 0; a--) {
+     if (a != i) {
+       document.getElementsByClassName("main--container-thirdBanner")[
+         a
+       ].style.display = "none";
+     }
+   }
+}
